db864df54904d46e663473f6f9e509f74a17cb11
Glitch (hackpack) <none>
2020-04-08 13:24:23 +0000

    Updated system.sh

--- system.sh
+++ system.sh
@@ -16,4 +16,7 @@ sleep 2
 echo Waiting for server [6]
 sleep 2
 echo Access Granted. # Wow awesome you hacked the server!!!!!!!!!!! omg!!!
+echo Run a command.
 read Data
+echo Data from: $Data # Now you can "hack" your friend. The $Data is the input you submitted.
+echo (insert something here) # The sky is the limit! Add more "echo"s for more lines.
