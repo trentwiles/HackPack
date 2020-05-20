ef0ee6ddfcbff44f74cb76f86d67819b5e72ed76
Glitch (hello-webpage) <none>
2020-05-20 13:43:56 +0000

    🎩⛩ Checkpoint ./hacks.c:9099011/1970

---
+++ hacks.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main( ) {
+
+   int c;
+  
+   printf( "Government Database. DO NOT ATTEMPT TO HACK/ :");
+   printf( "Enter your password :");
+   pass = getchar( );
+   if(c = )
+   printf( "\nYou entered: ");
+   putchar( c );
+
+   return 0;
+}
