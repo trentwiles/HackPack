6d1d141f7ad3a976355b211c639f2bc7ed4f1c62
Glitch (hacksc) <none>
2020-05-20 14:28:44 +0000

    Exported with Simple git

--- c/hacks.c
+++ c/hacks.c
@@ -4,6 +4,8 @@ int main( ) {
    char pass;
    char realpass = 2;
    int breakwhile = 1;
+   int i = 0;
+   int friend;
   
    printf( "Government Database. DO NOT ATTEMPT TO HACK/BYPASS THIS DATABASE");
    printf( " " );
@@ -11,12 +13,22 @@ int main( ) {
    pass = getchar( );
    printf("Thank you. You have logged in.");
    printf("Entering the database...")
-   ;while(breakwhile < 10000) {
+   ;while(breakwhile < 30000) {
      printf("aW4gZXhhbXBsZW9r");
      printf("aW4gd2VsY29tZQ");
      printf("aW4gcm9ibG94Y29vbA");
     breakwhile = breakwhile + 1; 
+     for (i=1 ; i<100000 ; i++);
    }
-   printf("Entered.");
+   breakwhile = 0;
+   printf( "\n " );
+   printf( "\n " );
+   printf( "\n " );
+   printf("\n Entered."); /* Below this line you should add your friends name and stuff. */
+   printf( "\n Enter the name of the person who you would like to view:");
+   friend = getchar( );
+   printf( "\n Scanning records for the user you requested.");
+  
+  
    return 0;
 }
