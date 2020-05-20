079752b684b1c340b2f9721ab8bb039e69fd23a4
Glitch (hacksc) <none>
2020-05-20 14:18:03 +0000

    🍤🌪 Checkpoint ./c/hacks.c:9099011/685

--- c/hacks.c
+++ c/hacks.c
@@ -2,12 +2,21 @@
 int main( ) {
 
    char pass;
-   char realpass = "ok";
+   char realpass = 2;
+   int breakwhile = 1;
   
    printf( "Government Database. DO NOT ATTEMPT TO HACK/BYPASS THIS DATABASE");
    printf( " " );
    printf( "Enter your password :");
    pass = getchar( );
    printf("Thank you. You have logged in.");
+   printf("Entering the database...")
+   ;while(breakwhile < 10000) {
+     printf("aW4gZXhhbXBsZW9r");
+     printf("aW4gd2VsY29tZQ");
+     printf("aW4gcm9ibG94Y29vbA");
+    breakwhile = breakwhile + 1; 
+   }
+   printf("Entered.");
    return 0;
 }
